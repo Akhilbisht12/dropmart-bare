b2bbea113b3bf249640630300e397b003db774a6
Akhilbisht12 <Akhilbisht83@gmail.com>
2021-05-12 13:15:39 +0530

    firebase packages and build successful

---
+++ Screens/Auth/PhoneAuth.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import auth from '@react-native-firebase/auth'
+
+const PhoneAuth = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default PhoneAuth
